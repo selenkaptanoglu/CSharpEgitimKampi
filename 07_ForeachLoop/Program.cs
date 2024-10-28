dcf2b30f7189f05751192e65e609f21db04fc9c5
Selen Kaptanoglu <selenkaptanoglu14@gmail.com>
2024-10-28 10:52:55 +0300

    Foreach Loop

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach Döngüsü
+
+            // -Değişken türü- -Değişken adı- in -Liste,Koleksiyon,Dizi-
+            //string[] cities = { "milano", "roma", "budapeşte", "ankara", "istanbul", "varşove" };
+            //foreach (string x in cities)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //int[] numbers = { 45, 78, 654, 366, 255, 63, 74, 89, 1242, 43533 };
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //int[] numbers = { 45, 78, 654, 366, 255, 63, 74, 89, 1242, 43533 };
+
+            //foreach (int number in numbers)
+            //{
+            //    if (number % 2 == 0)
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+            //}
+
+            //int[] numbers = { 45, 78, 654, 366, 255, 63, 74, 89, 1242, 43533 };
+
+            //int total = 0;
+            //foreach (int i in numbers)
+            //{
+            //    total += i;
+            //}
+            //Console.WriteLine(total);
+
+            //List<int> numbers = new List<int>()
+            //{
+            //    1,2,3,4,5,8
+            //};
+
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //string word = "Merhaba";
+            //foreach (char c in word)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            #endregion
+
+            #region Örnek Sınav Sistemi Uygulaması
+
+            Console.Write("***** C# Eğitim Kampı Sınavı Uygulaması *****");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Sınıftaki öğrenci sayısını kullanıcıdan alma
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Sınıfınızda kaç öğrenci var: ");
+            int studentCount=int.Parse(Console.ReadLine());
+            Console.WriteLine("---------------------------------------");
+
+            //Öğrenci isimlerini ve not ortalamalarını saklayacak diziler
+            string [] studentNames = new string[studentCount];
+            double [] studentExamAvg= new double[studentCount];
+
+            for(int i = 0; i < studentCount; i++) 
+            {
+                Console.Write($"{i+1}.öğrencinin ismini giriniz");
+                studentNames[i] = Console.ReadLine();
+
+                double totalExamResult = 0;
+
+                //Her öğrenci için 3 sınav notu girişi
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{studentNames[i]} isimli öğrencinin {j+1}. sınav notunu giriniz: ");
+                    double value=double.Parse(Console.ReadLine());
+                    totalExamResult += value;
+                }
+                studentExamAvg[i]= totalExamResult/3;
+            }
+            //Sınav ortalamaları
+            for(int i=0; i < studentCount; i++)
+            {
+                Console.WriteLine($"{studentNames[i]} adlı öğrencinin ortalaması: {studentExamAvg[i]}");
+
+                //Öğrencini ortalaması ve geçip kalma durumları
+                if (studentExamAvg >= 50)
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersi geçti");
+                }
+                else
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersten kaldı");
+                }
+                Console.WriteLine("------------------------------");
+            }
+
+
+
+
+
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
